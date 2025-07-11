086bbb680eefb872bdb82b6335857d6128cd5637
Jerome Verkyndt <j.verkyndt@students.ephec.be>
2025-07-12 01:53:13 +0200

    modif deploiment front

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,21 +1,18 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import api from '../api';
 
-axios.defaults.withCredentials = true;
 
 export default function useAuth() {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get('http://localhost:5001/api/auth/me', { withCredentials: true })
+        api.get('/auth/me')
             .then(res => {
                 setUser(res.data.user);
-                setLoading(false);
             })
             .catch(() => {
                 setUser(null);
-                setLoading(false);
             })
             .finally(() => setLoading(false));
     }, []);
