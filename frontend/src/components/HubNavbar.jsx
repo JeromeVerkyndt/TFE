2c0a00dae7484a27e67d004eab8827459cc78312
Jerome Verkyndt <j.verkyndt@students.ephec.be>
2025-07-14 02:34:50 +0200

    modif

--- frontend/src/components/HubNavbar.jsx
+++ frontend/src/components/HubNavbar.jsx
@@ -6,7 +6,7 @@ function NavbarHub() {
 
     const handleLogout = async () => {
         try {
-            await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/logout`, {}, { withCredentials: true });
+            await axios.post(`${import.meta.env.VITE_API_URL}/auth/logout`, {}, { withCredentials: true });
             navigate('/login');
         } catch (err) {
             console.error("Erreur lors de la déconnexion :", err);
